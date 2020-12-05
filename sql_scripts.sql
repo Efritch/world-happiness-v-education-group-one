4caa646c1bca650a27ca48e2c3c7e42c093165be
Efritch <70618234+Efritch@users.noreply.github.com>
2020-12-04 22:10:58 -0500

    added a file with sql scripts to create db tables.

---
+++ sql_scripts.sql
@@ -0,0 +1,31 @@
+CREATE TABLE world_happiness (
+	overall_rank INT,
+	country VARCHAR,
+	score FLOAT,
+	gdp_per_capita_score FLOAT,
+	social_support_score FLOAT,
+	healthy_life_expectancy_score FLOAT,
+	freedom_life_choices_score, FLOAT,
+	generosity FLOAT,
+	perceived_corruption_score FLOAT);
+	
+CREATE TABLE university_rankings (
+	year_2019 INT,
+	year_2018 INT,
+	institution_name VARCHAR,
+	country VARCHAR,
+	class_size VARCHAR,
+	academic_rep_score FLOAT,
+	academic_rep_rank INT,
+	employer_rep_score FLOAT,
+	employer_rep_rank INT,
+	faculty_student_score FLOAT,
+	faculty_student_rank INT,
+	faculty_citations_score FLOAT,
+	faculty_citations_rank INT,
+	intl_faculty_score FLOAT,
+	intl_faculty_rank INT,
+	intl_students_score FLOAT,
+	intl_students_rank INT,
+	overall_score FLOAT);
+
